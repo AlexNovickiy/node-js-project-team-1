ec1724fef98cb19dcac54613bef3bfb85c333330
Alina Slobodeniuk <112395038+AlinaSlobodeniuk@users.noreply.github.com>
2025-11-08 18:04:32 +0200

    get users pagination swagger

--- src/services/users.js
+++ src/services/users.js
@@ -1,8 +1,34 @@
 import { UsersCollection } from '../db/models/user.js';
 import { StoriesCollection } from '../db/models/story.js';
 
+export const getUsers = async (page, perPage, sortBy, sortOrder) => {
+  const skip = (page - 1) * perPage;
+
+  const [users, total] = await Promise.all([
+    UsersCollection.find()
+      .sort({ [sortBy]: sortOrder === 'asc' ? 1 : -1 })
+      .skip(skip)
+      .limit(perPage),
+    UsersCollection.countDocuments(),
+  ]);
+
+  const totalPages = Math.ceil(total / perPage);
+
+  return {
+    users,
+    pageInfo: {
+      total,
+      page,
+      perPage,
+      totalPages,
+      hasNextPage: page < totalPages,
+      hasPrevPage: page > 1,
+    },
+  };
+};
+
 export const getUserCurrentService = async (userId) => {
   return await UsersCollection.findOne({ _id: userId })
     .select('-password')
-    .populate('favorites'); 
+    .populate('favorites');
 };
