38553e021dd21366be153198d27aa000853b0185
Oksana-Horbachevska <oksanapaolo@gmail.com>
2025-11-09 15:42:52 +0100

    update swagger.json

--- src/controllers/stories.js
+++ src/controllers/stories.js
@@ -58,30 +58,20 @@ export const createStoryController = async (req, res, next) => {
 export const updateStoryController = async (req, res) => {
   let img;
   if (req.file) {
-    console.log('1. Починаю завантаження на Cloudinary...');
     const response = await saveFileToCloudinary(req.file);
-    console.log('2. Завантаження на Cloudinary завершено.'); // Якщо це не виводиться, проблема у saveFileToCloudinary
     img = response;
   }
   const storyData = {
     ...req.body,
-    // ...(img && { img }),
     img,
   };
-  console.log('3. Починаю оновлення MongoDB...');
+
   const result = await updateStory(req.params.storyId, req.user.id, storyData);
-  console.log('4. Оновлення MongoDB завершено.'); // Якщо це не виводиться, проблема у updateStory
+
   if (!result) {
     throw createHttpError(404, 'Story not found');
   }
-  // const data = {
-  //   message: 'Story PATCH endpoint placeholder',
-  //   storyId: req.params.storyId,
-  //   body: req.body,
-  //   file: req.file,
-  //   userId: req.user.id,
-  // };
-  console.log('5. Відправляю відповідь.'); // Якщо це виводиться, але клієнт зависає, проблема в налаштуваннях Express
+
   res.status(200).json({
     status: 200,
     message: 'Successfully patched a story!',
