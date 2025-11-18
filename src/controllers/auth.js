8e4fb2a53d7599e514f3d339f410d1b55f2b27ef
ArturMykhalyk <artuhan2015@gmail.com>
2025-11-18 23:11:01 +0200

    test

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -95,7 +95,7 @@ export const refreshController = async (req, res) => {
 export const requestResetEmailController = async (req, res) => {
   await requestResetToken(req.body.email);
   res.json({
-    message: 'Reset password email was successfully sent!',
+    message: 'Reset password email was successfully sent',
     status: 200,
     data: {},
   });
